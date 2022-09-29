ff1857f5f5ad40228f84d60fdd748854904e442c
John Kasun <johnkasun@live.com>
2022-09-28 21:00:07 -0400

    Start implementing DelayTankEngine process function

--- src/lib_DelayTankEngine/DelayTankEngine.cpp
+++ src/lib_DelayTankEngine/DelayTankEngine.cpp
@@ -11,3 +11,45 @@ DelayTankEngine::~DelayTankEngine()
 {
 	mDelays.clear();
 }
+
+void DelayTankEngine::setDelay(int index, float delayInSeconds)
+{
+	mDelays.at(index)->setDelay(delayInSeconds);
+}
+
+void DelayTankEngine::setGain(int index, float gain)
+{
+	mDelays.at(index)->setGain(gain);
+}
+
+void DelayTankEngine::setPan(int index, int pan)
+{
+	mDelays.at(index)->setPan(pan);
+}
+
+std::pair<float, float> DelayTankEngine::process(float input)
+{
+	auto outputSumL = 0.0f;
+	auto outputSumR = 0.0f;
+	for (int i = 0; i < mNumActiveDelays; i++) {
+		auto output = mDelays.at(i)->process(input);
+		outputSumL += output.first;
+		outputSumR += output.second;
+	}
+	return std::make_pair(outputSumL, outputSumR);
+}
+
+float DelayTankEngine::getDelay(int index) const
+{
+	return mDelays.at(index)->getDelay();
+}
+
+float DelayTankEngine::getGain(int index) const
+{
+	return mDelays.at(index)->getGain();
+}
+
+int DelayTankEngine::getPan(int index) const
+{
+	return mDelays.at(index)->getPan();
+}
--- src/lib_DelayTankEngine/include/DelayTankEngine.h
+++ src/lib_DelayTankEngine/include/DelayTankEngine.h
@@ -11,6 +11,16 @@ class DelayTankEngine {
 public:
 	DelayTankEngine(float maxDelayInSeconds, int maxNumDelays, float sampleRate);
 	~DelayTankEngine();
+
+	void setDelay(int index, float delayinSeconds);
+	void setGain(int index, float gain);
+	void setPan(int index, int pan);
+	std::pair<float, float> process(float input);
+
+	float getDelay(int index) const;
+	float getGain(int index) const;
+	int getPan(int index) const;
 private:
 	std::vector<std::unique_ptr<Delay>> mDelays;
+	int mNumActiveDelays;
 };
